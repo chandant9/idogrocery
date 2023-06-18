239533a71db352a2e01e400047b50f8ba37fbe39
chandant9 <discoverchan9@gmail.com>
2023-06-18 14:40:09 -0300

    added dotenv and decouple to increase security

--- grocery_app/grocery_app/settings.py
+++ grocery_app/grocery_app/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+config.load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)sy_8e14y7b#mqqh^v4dna4+c+%rfvj31r=7hiv2f)=_z2+s3!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
